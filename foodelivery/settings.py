8528570cedd17847a05abeec5c7cf60213884ac5
mbayedione10 <mbayedione10@gmail.com>
2021-07-30 11:49:16 +0000

    set staticfiles dirs and add host

--- foodelivery/settings.py
+++ foodelivery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^4ccleu04=)9tc=fff_j!5q-rbd=p5n#bm^crkq6_uh$3hyvah'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -150,6 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
