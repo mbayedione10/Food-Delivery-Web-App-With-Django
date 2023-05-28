5048919eac14300ab81810227adf4d24ee583369
mbayedione10 <mbayedione10@gmail.com>
2023-05-28 04:05:57 +0000

    add all statics + settings

--- foodelivery/settings.py
+++ foodelivery/settings.py
@@ -152,6 +152,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    # '/customer/',
+    # '/restaurant/',
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
